61830a2fe315e7622e674d83983ecf1f2323d947
Maciej Borkowski <borkowski.mac@gmail.com>
2020-07-11 10:50:43 +0200

    Add scrapedurl url.

--- webscraper/scraper/views.py
+++ webscraper/scraper/views.py
@@ -1,1 +1,10 @@
 from rest_framework import viewsets
+from scraper.serializers import ScrapedUrlSerializer
+from scraper.models import ScrapedUrl
+
+
+class ScrappedUrlViewSet(viewsets.ModelViewSet):
+    queryset = ScrapedUrl.objects.all()
+    serializer_class = ScrapedUrlSerializer
+
+
--- webscraper/webscraper/urls.py
+++ webscraper/webscraper/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
+from scraper import views
 
+router = routers.DefaultRouter()
+router.register(r'scrapped_url', views.ScrappedUrlViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
