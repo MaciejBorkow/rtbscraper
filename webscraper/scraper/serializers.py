b24f8112ac5cae086eedefa9144dbae48c8338b7
Maciej Borkowski <borkowski.mac@gmail.com>
2020-07-11 10:49:38 +0200

    Add scrapedurl serializer.

--- webscraper/scraper/serializers.py
+++ webscraper/scraper/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from scraper.models import ScrapedUrl
+
+
+class ScrapedUrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScrapedUrl
+        fields = ['id', 'task_status', 'url', 'data']
+        read_only_fields = ['task_status', 'data']
