162ce2d333fe52a0e712a85cae2c58aae7418726
Maciej Borkowski <borkowski.mac@gmail.com>
2020-07-11 10:49:06 +0200

    Add scrapedurl model.

--- webscraper/scraper/models.py
+++ webscraper/scraper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 
+class ScrapedUrl(models.Model):
+    task_id = models.CharField(max_length=50, blank=True, default='')
+    task_status = models.CharField(max_length=10, blank=True, default='')
+    url = models.fields.URLField()
+    data = models.fields.TextField(blank=True, default='')
