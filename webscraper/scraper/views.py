99bbbc5cb6e6c96302ad14244d1fbde23e4a33a3
Maciej Borkowski <borkowski.mac@gmail.com>
2020-07-11 11:02:23 +0200

    Add scrapedurl view

--- webscraper/scraper/views.py
+++ webscraper/scraper/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from scraper.serializers import ScrapedUrlSerializer
+from scraper.tasks import scrap_url
 from scraper.models import ScrapedUrl
 
 
@@ -7,4 +8,9 @@ class ScrappedUrlViewSet(viewsets.ModelViewSet):
     queryset = ScrapedUrl.objects.all()
     serializer_class = ScrapedUrlSerializer
 
-
+    def perform_create(self, serializer):
+        serializer.save()
+        celery_task = scrap_url.delay(serializer.instance.id)
+        serializer.instance.task_status = celery_task.status
+        serializer.instance.task_id = celery_task.id
+        serializer.instance.save()
