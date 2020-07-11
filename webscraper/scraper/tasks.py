744a473cff26cf194bd2c1e482b17b75585e5a79
Maciej Borkowski <borkowski.mac@gmail.com>
2020-07-11 14:31:52 +0200

    Add decorator to keep object task state up to date.

--- webscraper/scraper/tasks.py
+++ webscraper/scraper/tasks.py
@@ -7,13 +7,20 @@ from scraper.models import ScrapedUrl
 from scraper.html_filters import example_html_filter
 
 
-@app.task(bind=True)
-def scrap_url(self, pk: int, url: str):
-    status = self.AsyncResult(self.request.id).state
-    ScrapedUrl.objects.filter(id=pk).update(task_status=status)
+def state_up_to_date(func):
+    def wrapper(*args, **kwargs):
+        self = args[0]
+        pk = kwargs['pk']
+        status = self.AsyncResult(self.request.id).state
+        ScrapedUrl.objects.filter(id=pk).update(task_status=status)
+        func(*args, **kwargs)
+        ScrapedUrl.objects.filter(id=pk).update(task_status='SUCCESS')
+    return wrapper
 
+
+@app.task(bind=True)
+@state_up_to_date
+def scrap_url(self, *, pk: int, url: str):
     html_text = requests.get(url).text
     html_text = example_html_filter(html_text)
     ScrapedUrl.objects.filter(id=pk).update(data=html_text)
-
-    ScrapedUrl.objects.filter(id=pk).update(task_status='SUCCESS')
